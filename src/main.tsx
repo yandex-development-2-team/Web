448915185fc81d037ca0fc9c2f6edfd9347b9518
Alexander Venedyukhin <alexandervenedyukhin@alexanders-mac-mini.taila8eceb.ts.net>
2026-02-04 18:59:48 -0800

    feat(main): подключен глобальный Toaster от Sonner для отображения уведомлений

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
 
 import '@/styles/globals.css';
 import App from '@/App';
+import { Toaster } from 'sonner';
+import { store } from '@/store';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+      <Toaster richColors position="top-right" />
+    </Provider>
   </StrictMode>,
 );
