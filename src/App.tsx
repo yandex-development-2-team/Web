19b7ec31059f12057335b28ec353e9761157179e
Smirnov Nikita <nikitaloko@mail.ru>
2026-02-05 23:09:23 +0300

    Внес изменения согласно замечаниям

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,10 @@
 import { cn } from '@/utils';
-import Button from './components/ui/Button';
-import Input from './components/ui/Input';
-import Textarea from './components/ui/Textarea';
 
 function App() {
   return (
     <div className="p-5">
       <h1 className={cn('text-brand mb-4 bg-violet-300')}>
         Админ панель. Яндекс развитие Проверка тестовых компонентов
-        <Button onClick={() => console.log('Тестовая кнопка')}>
-          Тестовая кнопка
-        </Button>
-        <hr />
-        <Input onChange={() => console.log('Тестовый инпут')} />
-        <hr />
-        <Textarea onChange={() => console.log('Тестовый TextArea')} />
       </h1>
     </div>
   );
--- src/components/ui/Textarea/Textarea.tsx
+++ src/components/ui/Textarea/Textarea.tsx
@@ -1,8 +1,7 @@
-import * as React from 'react';
-
+import { type ComponentProps } from 'react';
 import { cn } from '@/utils/index';
 
-function Textarea({ className, ...props }: React.ComponentProps<'textarea'>) {
+function Textarea({ className, ...props }: ComponentProps<'textarea'>) {
   return (
     <textarea
       data-slot="textarea"
