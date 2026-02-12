931cddbc621adb9d7b03968712c556b8188b1260
Andrey Ignashov <maylo7771@yandex.ru>
2026-02-13 02:51:22 +0300

    feat: добавить варианты button

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -1,36 +1,44 @@
 import { type ComponentProps } from 'react';
 import { cva, type VariantProps } from 'class-variance-authority';
 import { Slot } from 'radix-ui';
-import { cn } from '@/utils/index';
+
+import { cn } from '@/utils';
+
 const buttonVariants = cva(
-  "focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive inline-flex shrink-0 items-center justify-center gap-2 rounded-md text-sm font-medium whitespace-nowrap transition-all outline-none focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+  cn(
+    'button-text inline-flex w-min shrink-0 cursor-pointer items-center justify-center gap-3 rounded-lg whitespace-nowrap outline-2 outline-transparent transition-all disabled:pointer-events-none',
+    'focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[2px]',
+    'aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive',
+    "[&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg]:stroke-1 [&_svg:not([class*='size-'])]:size-6",
+  ),
   {
     variants: {
       variant: {
-        default: 'bg-primary text-primary-foreground hover:bg-primary/90',
-        destructive:
-          'bg-destructive hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60 text-white',
-        outline:
-          'bg-background hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50 border shadow-xs',
-        secondary:
-          'bg-secondary text-secondary-foreground hover:bg-secondary/80',
+        ['default-primary']:
+          'bg-primary hover:bg-accent active:bg-accent-strong disabled:bg-muted disabled:text-muted-foreground',
+        ['default-secondary']:
+          'bg-secondary text-secondary-foreground hover:border-accent border-primary active:outline-accent-strong disabled:border-muted disabled:text-muted-foreground border',
+        ['danger-primary']:
+          'bg-destructive text-secondary hover:bg-destructive-light active:bg-chart-2 disabled:bg-muted disabled:text-muted-foreground',
+        ['danger-secondary']:
+          'bg-secondary border-destructive-light text-destructive hover:border-destructive-light/50 active:border-chart-2 disabled:border-muted disabled:text-muted-foreground border-2 disabled:border',
+        shadow:
+          'bg-card hover:border-accent active:bg-primary justify-between border border-transparent shadow-[0_1px_3px_1px_rgba(0,0,0,0.15),0_1px_2px_0_rgba(0,0,0,0.3)]',
+        link: 'text-foreground border-b-primary hover:border-b-accent-strong rounded-none border-b-1 active:shadow-[inset_0_-2px_0_0_var(--accent-strong)]',
         ghost:
-          'hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50',
-        link: 'text-primary underline-offset-4 hover:underline',
+          'hover:[&_svg]:stroke-accent-strong active:[&_svg]:stroke-pending-foreground disabled:[&_svg]:stroke-border border-none bg-none outline-none',
       },
       size: {
-        default: 'h-9 px-4 py-2 has-[>svg]:px-3',
-        xs: "h-6 gap-1 rounded-md px-2 text-xs has-[>svg]:px-1.5 [&_svg:not([class*='size-'])]:size-3",
-        sm: 'h-8 gap-1.5 rounded-md px-3 has-[>svg]:px-2.5',
-        lg: 'h-10 rounded-md px-6 has-[>svg]:px-4',
-        icon: 'size-9',
-        'icon-xs': "size-6 rounded-md [&_svg:not([class*='size-'])]:size-3",
-        'icon-sm': 'size-8',
-        'icon-lg': 'size-10',
+        default: 'px-8 py-3',
+        ['icon-md']: "size-10 [&_svg:not([class*='size-'])]:size-6",
+        ['icon-lg']: "size-12 [&_svg:not([class*='size-'])]:size-8",
+        ['icon-xl']: "h-12 w-20 [&_svg:not([class*='size-'])]:size-8",
+        shadow: 'p-3',
+        link: 'p-0 pb-1',
       },
     },
     defaultVariants: {
-      variant: 'default',
+      variant: 'default-primary',
       size: 'default',
     },
   },
@@ -38,7 +46,7 @@ const buttonVariants = cva(
 
 function Button({
   className,
-  variant = 'default',
+  variant = 'default-primary',
   size = 'default',
   asChild = false,
   ...props
