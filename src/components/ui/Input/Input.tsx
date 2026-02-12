44b6417470b58fbac20b9a332bf1dbbfdce9c4d2
Andrey Ignashov <maylo7771@yandex.ru>
2026-02-13 02:46:46 +0300

    feat: добавить обертку для input

--- src/components/ui/Input/Input.tsx
+++ src/components/ui/Input/Input.tsx
@@ -1,19 +1,48 @@
-import { type ComponentProps } from 'react';
-import { cn } from '@/utils/index';
+import { useId, type ComponentProps } from 'react';
 
-function Input({ className, type, ...props }: ComponentProps<'input'>) {
+import { cn } from '@/utils';
+import { Label } from '@/components/ui/Label';
+
+interface InputProps extends ComponentProps<'input'> {
+  label?: string;
+  error?: string;
+}
+
+function Input({
+  className,
+  type = 'text',
+  id,
+  label,
+  error,
+  ...props
+}: InputProps) {
+  const gegerateId = useId();
+  const inputId = id || gegerateId;
+
   return (
-    <input
-      type={type}
-      data-slot="input"
-      className={cn(
-        'file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
-        'focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]',
-        'aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive',
-        className,
-      )}
-      {...props}
-    />
+    <div className={cn('flex min-w-18 flex-col gap-1')}>
+      <Label htmlFor={inputId}>{label}</Label>
+      <div className={cn('relative')}>
+        <input
+          id={inputId}
+          type={type}
+          data-slot="input"
+          className={cn(
+            'bg-card h-11 w-full min-w-0 rounded-lg p-3',
+            'border-border border',
+            'placeholder:text-border placeholder:text-[14px] placeholder:italic',
+            'file:text-foreground file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium',
+            'selection:bg-primary selection:text-primary-foreground transition-[color,box-shadow] outline-none',
+            'focus-visible:border-ring focus-visible:ring-ring/50 focus:placeholder:text-ring focus:border-muted-foreground',
+            'disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50',
+            'aria-invalid:ring-destructive/20 aria-invalid:border-destructive',
+            className,
+          )}
+          {...props}
+        />
+      </div>
+      {error && <div className={cn('xxs-text text-error')}>{error}</div>}
+    </div>
   );
 }
 
