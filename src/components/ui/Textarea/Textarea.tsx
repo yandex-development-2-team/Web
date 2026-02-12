67ae18ed4cecf026a1c87bc76106349b9c91545a
Andrey Ignashov <maylo7771@yandex.ru>
2026-02-13 02:48:32 +0300

    feat: добавить обертку для textarea

--- src/components/ui/Textarea/Textarea.tsx
+++ src/components/ui/Textarea/Textarea.tsx
@@ -1,16 +1,36 @@
-import { type ComponentProps } from 'react';
-import { cn } from '@/utils/index';
+import { useId, type ComponentProps } from 'react';
 
-function Textarea({ className, ...props }: ComponentProps<'textarea'>) {
+import { cn } from '@/utils';
+import { Label } from '@/components/ui/Label';
+
+interface TextareaProps extends ComponentProps<'textarea'> {
+  label?: string;
+  error?: string;
+}
+
+function Textarea({ className, id, label, error, ...props }: TextareaProps) {
+  const gegerateId = useId();
+  const textareaId = id || gegerateId;
+
   return (
-    <textarea
-      data-slot="textarea"
-      className={cn(
-        'border-input placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 flex field-sizing-content min-h-16 w-full rounded-md border bg-transparent px-3 py-2 text-base shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
-        className,
-      )}
-      {...props}
-    />
+    <div className={cn('flex flex-col gap-1')}>
+      <Label htmlFor={textareaId}>{label}</Label>
+      <textarea
+        data-slot="textarea"
+        id={textareaId}
+        className={cn(
+          'bg-card flex field-sizing-content min-h-15 w-full rounded-lg p-3',
+          'border-border border',
+          'placeholder:text-border placeholder:text-[14px] placeholder:italic',
+          'focus-visible:border-ring focus-visible:ring-ring/50 focus:border-muted-foreground focus:placeholder:text-ring',
+          'aria-invalid:ring-error/20 aria-invalid:border-error',
+          'text-foreground transition-[color,box-shadow] outline-none disabled:cursor-not-allowed disabled:opacity-50',
+          className,
+        )}
+        {...props}
+      />
+      {error && <div className={cn('xxs-text text-error')}>{error}</div>}
+    </div>
   );
 }
 
