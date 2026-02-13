c1e67635cdb986f93e655a5453cd70daed5ce0ee
Koblov Andrey <a.coblov@yandex.ru>
2026-02-13 22:36:16 +0400

    fix app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import { Outlet } from 'react-router';
 import { AppSidebar, SidebarInset } from './components/shell/Sidebar';
 import { SidebarProvider } from './app/providers/Sidebar';
-import HomeIcon2 from '@/assets/icons/home.svg?react';
 function App() {
   return (
     <SidebarProvider>
       <AppSidebar />
 
       <SidebarInset>
-        <HomeIcon2 className='h-15 w-15'/>
         <Outlet />
       </SidebarInset>
     </SidebarProvider>
