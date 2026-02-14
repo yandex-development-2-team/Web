a4e80e0b8b7f7d10b5132499a56ed30fd0df0da1
Alexander Venedyukhin <alexandervenedyukhin@alexanders-mac-mini.taila8eceb.ts.net>
2026-02-14 00:17:54 -0800

    fix: обновлены shadcn/ui components (button, card, input)

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,5 +1,4 @@
 import * as React from "react"
-
 import { cn } from "@/utils/index"
 
 function Input({ className, type, ...props }: React.ComponentProps<"input">) {
@@ -8,9 +7,11 @@ function Input({ className, type, ...props }: React.ComponentProps<"input">) {
       type={type}
       data-slot="input"
       className={cn(
-        "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-        "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
-        "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+        "w-full border bg-input px-3 text-foreground placeholder:text-border placeholder:text-sm  placeholder:italic",
+        "transition-all outline-none",
+        "focus-visible:ring-0 focus-visible:border-ring",
+        "aria-invalid:border-destructive aria-invalid:ring-0",
+        "disabled:cursor-not-allowed disabled:opacity-50",
         className
       )}
       {...props}
