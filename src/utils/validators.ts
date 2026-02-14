7835a38f2a7f2c4bb4df844f4886b242d6760fcd
Alexander Venedyukhin <alexandervenedyukhin@alexanders-mac-mini.taila8eceb.ts.net>
2026-02-14 11:56:16 -0800

    fix: исправлен import в validators.ts

--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -1,9 +1,8 @@
-import * as z from 'zod'
+import { z } from 'zod'
 
 export const loginSchema = z.object({
   login: z.string().min(1, 'Логин обязателен'),
-  password: z
-    .string()
+  password: z.string()
     .min(8, 'Пароль должен содержать минимум 8 символов')
     .regex(/[A-Za-z]/, 'Пароль должен содержать хотя бы одну букву')
     .regex(/[0-9]/, 'Пароль должен содержать хотя бы одну цифру'),
