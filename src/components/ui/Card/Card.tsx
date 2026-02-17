e1fd5c6140e2132c7061e75814ebdd996caa85d2
Alexander Venedyukhin <alexandervenedyukhin@alexanders-mac-mini.taila8eceb.ts.net>
2026-02-17 01:34:41 -0800

    fix: исправлен импорт ComponentProps

--- src/components/ui/Card/Card.tsx
+++ src/components/ui/Card/Card.tsx
@@ -1,13 +1,21 @@
-import * as React from "react"
-import { cn } from "@/utils/index"
+import { type ComponentProps } from 'react'
+import { cn } from '@/utils'
 
-function Card({ className, variant = "flat", ...props }: React.ComponentProps<"div"> & { variant?: "default" | "flat" }) {
+type CardProps = ComponentProps<'div'> & {
+  variant?: 'default' | 'flat'
+}
+
+function Card({
+  className,
+  variant = 'flat',
+  ...props
+}: CardProps) {
   return (
     <div
       className={cn(
-        variant === "default"
-          ? "bg-card text-card-foreground rounded-xl border shadow-sm"
-          : "bg-card text-card-foreground border-0 rounded-none shadow-none",
+        variant === 'default'
+          ? 'bg-card text-card-foreground rounded-xl border shadow-sm'
+          : 'bg-card text-card-foreground border-0 rounded-none shadow-none',
         className
       )}
       {...props}
@@ -15,16 +23,40 @@ function Card({ className, variant = "flat", ...props }: React.ComponentProps<"d
   )
 }
 
-function CardHeader({ className, ...props }: React.ComponentProps<"div">) {
-  return <div className={cn("px-6 grid auto-rows-min", className)} {...props} />
+function CardHeader({
+  className,
+  ...props
+}: ComponentProps<'div'>) {
+  return (
+    <div
+      className={cn('px-6 grid auto-rows-min', className)}
+      {...props}
+    />
+  )
 }
 
-function CardTitle({ className, ...props }: React.ComponentProps<"div">) {
-  return <div className={cn("leading-normal font-bold text-[32px]", className)} {...props} />
+function CardTitle({
+  className,
+  ...props
+}: ComponentProps<'div'>) {
+  return (
+    <div
+      className={cn('leading-normal font-bold text-[32px]', className)}
+      {...props}
+    />
+  )
 }
 
-function CardContent({ className, ...props }: React.ComponentProps<"div">) {
-  return <div className={cn("px-6", className)} {...props} />
+function CardContent({
+  className,
+  ...props
+}: ComponentProps<'div'>) {
+  return (
+    <div
+      className={cn('px-6', className)}
+      {...props}
+    />
+  )
 }
 
 export { Card, CardHeader, CardTitle, CardContent }
