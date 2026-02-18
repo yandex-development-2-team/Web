cbbce174495cc288370ab7ae1a141704bc318f23
Alexander Venedyukhin <alexandervenedyukhin@alexanders-mac-mini.taila8eceb.ts.net>
2026-02-17 22:20:44 -0800

    fix: доработана LoginPage min-h

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -3,7 +3,7 @@ import loginBg from '@/assets/images/login-page-cover.png'
 
 export default function LoginPage() {
   return (
-    <div className="h-svh w-full bg-background px-[30px] py-5 space-x-5 flex">
+    <div className="h-svh min-h-[445px] w-full bg-background px-[30px] py-5 space-x-5 flex">
       <div className="flex min-w-[445px] max-w-[445px] flex-shrink-0 h-full relative">
         <div className="flex w-full flex-col items-center justify-center p-10 bg-secondary rounded-lg">
           <div className="absolute left-17 top-6 text-left">
