0a16cef918575200a6ff853fa0bb0cab7d7ef335
Koblov Andrey <a.coblov@yandex.ru>
2026-02-27 13:04:35 +0400

    fix: Удалено лишнее

--- src/pages/AnalyticsPage/AverageBoxAttendancePage/AverageBoxAttendancePage.tsx
+++ src/pages/AnalyticsPage/AverageBoxAttendancePage/AverageBoxAttendancePage.tsx
@@ -1,54 +1,5 @@
-import { DataTable } from '@/components/ui/DataTable';
-import { SearchBarTable } from '@/components/ui/DataTable/ui/SearchBarTable';
-import { MetricCard } from '@/components/ui/MetricCard';
-import {
-  COLUMNS_ANALYTIC_USER,
-  DATA_ANALYTIC_USER,
-} from '@/mock/table/mock-analitic-users';
-import { useState } from 'react';
-
 const AverageBoxAttendancePage = () => {
-  const [isLoading] = useState(false);
-  const [isError] = useState(false);
-  const [filter, setFilter] = useState('');
-
-  return (
-    <div className="flex h-screen flex-col gap-5">
-      <div className="m-5 mb-0 rounded-lg bg-(--color-card)">
-        <h2 className="m-5 mb-0">Сводка дня по заявкам</h2>
-        <div className="m-5 mt-0 flex flex-row gap-5">
-          <MetricCard label="Принятые заявки" metric={29}></MetricCard>
-          <MetricCard label="Реализованные" metric={25}></MetricCard>
-          <MetricCard
-            label="Нереализованные"
-            metric={4}
-            variant="danger"
-          ></MetricCard>
-        </div>
-      </div>
-      <div className="m-5 mt-0 rounded-lg bg-(--color-card)">
-        <div className="m-5 mb-8 flex flex-row gap-3">
-          <SearchBarTable value={filter} onChange={setFilter} />
-        </div>
-        <DataTable
-          columns={COLUMNS_ANALYTIC_USER}
-          data={DATA_ANALYTIC_USER}
-          isLoading={isLoading}
-          isError={isError}
-          defaultRowCount={5}
-          rowKey={'id'}
-          filter={filter}
-          showControls="pagination"
-          rowSelected={{
-            enabled: true,
-            onChange(ids) {
-              console.log(ids);
-            },
-          }}
-        />
-      </div>
-    </div>
-  );
+  return <h1>В разработке...</h1>;
 };
 
 export const Component = AverageBoxAttendancePage;
--- src/pages/AnalyticsPage/UsersAnalyticsPage/UsersAnalyticsPage.tsx
+++ src/pages/AnalyticsPage/UsersAnalyticsPage/UsersAnalyticsPage.tsx
@@ -1,58 +1,5 @@
-import { UploadArrowIcon } from '@/assets/icons';
-import { Button } from '@/components/ui/Button';
-import { DataTable } from '@/components/ui/DataTable';
-import { MetricCard } from '@/components/ui/MetricCard/MetricCard';
-import {
-  COLUMNS_ANALYTIC_USER,
-  DATA_ANALYTIC_USER,
-} from '@/mock/table/mock-analitic-users';
-import { useState } from 'react';
-
 const UsersAnalyticsPage = () => {
-  const [isLoading] = useState(false);
-  const [isError] = useState(false);
-
-  return (
-    <div className="m-5 flex flex-col gap-5">
-      <div className="rounded-lg bg-(--color-card)">
-        <div className="m-5 mb-4 flex flex-col gap-4">
-          <h2>Аналитика пользователей</h2>
-          <h3 className="font-semibold">Сводка</h3>
-        </div>
-        <div className="m-5 mt-0 flex flex-row gap-5">
-          <MetricCard label="Зашли в бот" metric={150}></MetricCard>
-          <MetricCard label="Подали заявку" metric={120}></MetricCard>
-          <MetricCard
-            label="Просмотры заявки"
-            metric={30}
-            variant="danger"
-          ></MetricCard>
-        </div>
-      </div>
-      <div className="rounded-lg bg-(--color-card)">
-        <div className="m-5 mb-8 flex flex-row gap-3">
-          <Button className="w-42">Выбрать</Button>
-          <Button
-            variant={'shadow'}
-            className="- h-11.5 w-11.5 border border-(--color-border) p-2.75 shadow-none"
-          >
-            <UploadArrowIcon className="size-6" />
-          </Button>
-        </div>
-        <DataTable
-          data={DATA_ANALYTIC_USER}
-          columns={COLUMNS_ANALYTIC_USER}
-          rowKey="id"
-          isLoading={isLoading}
-          isError={isError}
-          rowSelected={{ enabled: true }}
-          onDeleteRow={() => console.log('')}
-          onEditRow={() => console.log('')}
-          onDownloadRow={() => console.log('')}
-        />
-      </div>
-    </div>
-  );
+  return <h1>В разработке...</h1>;
 };
 
 export const Component = UsersAnalyticsPage;
