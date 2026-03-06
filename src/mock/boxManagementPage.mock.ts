6358808f9d39bfdad837fdf3c10812acf9500c51
Andrey Ignashov <maylo7771@yandex.ru>
2026-03-06 17:44:38 +0300

    feat: добавить mock data

---
+++ src/mock/boxManagementPage.mock.ts
@@ -0,0 +1,79 @@
+export type StatisticItemType = {
+  id: string;
+  title: string;
+  count: string;
+  isHighlighted?: boolean;
+};
+
+export const NOT_IMPLEMENTED_ID = 'not-implemented';
+
+export const STATISTIC_OF_DAY: StatisticItemType[] = [
+  {
+    id: 'created',
+    title: 'Созданные',
+    count: '5',
+  },
+  {
+    id: 'in-work',
+    title: 'В работе',
+    count: '25',
+  },
+  {
+    id: 'implemented',
+    title: 'Реализованные',
+    count: '10',
+  },
+  {
+    id: 'not-implemented',
+    title: 'Не реализованные',
+    count: '4',
+    isHighlighted: true,
+  },
+];
+
+export type ProjectItem = {
+  id: string;
+  name: string;
+};
+
+export const BOXES_AND_SPECPROJECTS_LIST: {
+  boxes: ProjectItem[];
+  specialProjects: ProjectItem[];
+} = {
+  boxes: [
+    {
+      id: 'box_1',
+      name: 'Название коробки',
+    },
+    {
+      id: 'box_2',
+      name: 'Название коробки',
+    },
+    {
+      id: 'box_3',
+      name: 'Название коробки',
+    },
+    {
+      id: 'box_4',
+      name: 'Название коробки',
+    },
+  ],
+  specialProjects: [
+    {
+      id: 'specproject_1',
+      name: 'Название спецпроекта',
+    },
+    {
+      id: 'specproject_2',
+      name: 'Название спецпроекта',
+    },
+    {
+      id: 'specproject_3',
+      name: 'Название спецпроекта',
+    },
+    {
+      id: 'specproject_4',
+      name: 'Название спецпроекта',
+    },
+  ],
+};
