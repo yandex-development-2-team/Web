9edd2a7156c0c9a6717c4a5308fdf450f07c5fdd
Andrey Ignashov <maylo7771@yandex.ru>
2026-03-18 15:57:16 +0300

    feat: добавить input file

--- src/pages/AnalyticsPage/AnalyticsDynamicPage.tsx
+++ src/pages/AnalyticsPage/AnalyticsDynamicPage.tsx
@@ -4,14 +4,25 @@ import { Button } from '@/components/ui/Button';
 import { DeleteModal } from '@/components/ui/Modal';
 import { cn } from '@/utils';
 import { useModal } from '@/hooks';
+import { Input } from '@/components/ui/Input';
+import { useRef } from 'react';
+import { useFileUpload } from '@/hooks';
+import { useQuery } from '@tanstack/react-query';
+import { fetchFileList } from '@/services/uploadFile.service';
 
 const DEFAULT_PAGE_DATA = {
   title: 'Рабочие моментики',
 };
 
 export function AnalyticDynamicPage() {
+  const inputRef = useRef<HTMLInputElement | null>(null);
   const { isOpen, open, close } = useModal();
   const { id } = useParams();
+  const { handleFileChange } = useFileUpload();
+  const loadFileList = useQuery({
+    queryKey: ['files', 'list'],
+    queryFn: fetchFileList,
+  });
 
   return (
     <div className="flex h-full flex-col justify-between gap-5">
@@ -20,15 +31,26 @@ export function AnalyticDynamicPage() {
           <h2>{DEFAULT_PAGE_DATA.title}</h2>
         </div>
         <div className="bg-card flex flex-col gap-8 rounded-lg p-5">
-          <p className="text-[24px] font-normal">
-            В этом блоке пока нет данных, добавьте первый элемент, чтобы начать
-            работу
-          </p>
-          <div className="flex items-center gap-4">
-            <Button size={'icon-lg'}>
+          {!loadFileList.data && (
+            <p className="text-[24px] font-normal">
+              В этом блоке пока нет данных, добавьте первый элемент, чтобы
+              начать работу
+            </p>
+          )}
+          <div className="button-text border-b-muted relative flex max-w-103 items-center gap-3 border-b p-3">
+            <Button size={'icon-lg'} onClick={() => inputRef.current?.click()}>
               <PlusIcon />
             </Button>
             Добавить файл
+            <div className="absolute">
+              <Input
+                type="file"
+                hidden
+                ref={inputRef}
+                accept=".pdf,.doc,.docx,.xls,.xlsx"
+                onChange={handleFileChange}
+              />
+            </div>
           </div>
         </div>
       </div>
@@ -36,8 +58,8 @@ export function AnalyticDynamicPage() {
         itemId={id ? id : ''}
         isOpen={isOpen}
         onClose={close}
-        deletePath=""
-        onConfirm={() => console.log('block delete')}
+        deletePath={`/api/v1/file/${id}`}
+        onConfirm={() => (id ? true : false)}
       />
       <Button variant={'danger-secondary'} className="ml-auto" onClick={open}>
         Удалить блок
