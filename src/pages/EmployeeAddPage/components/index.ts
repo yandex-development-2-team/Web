c0e387b2cbbdd2577abe5b930d55bb05d9aa2277
Andrey Ignashov <maylo7771@yandex.ru>
2026-03-19 15:46:36 +0300

    fix(employeeAddPage): исправить путь

--- src/pages/EmployeeAddPage/components/index.ts
+++ src/pages/EmployeeAddPage/components/index.ts
@@ -5,5 +5,5 @@ export { PersonalSection } from './PersonalSection';
 export { ContactSection } from './ContactSection';
 export { PositionSection } from './PositionSection';
 export { AccessSection } from './AccessSection';
-export { default as EmployeesIcon } from '@/assets/icons/mdi:user-outline.svg?react';
+export { default as EmployeesIcon } from '@/assets/icons/mdi-user-outline.svg?react';
 export { default as UploadIcon } from '@/assets/icons/upload-32.svg?react';
